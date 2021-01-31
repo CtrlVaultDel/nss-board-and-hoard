2e7c12475498eb7ed38743b9982a38e228c2a569
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-01-31 14:24:04 -0600

    Added number beside range slider that updates as the slider is changed

--- src/components/boardGameAtlas/GameProvider.js
+++ src/components/boardGameAtlas/GameProvider.js
@@ -1,22 +1,28 @@
 import React, { useState, createContext } from "react";
+
+// API key for Board Game Atlas (BGA) fetch calls 
+// Note: (key.js file is also inside .gitignore)
 import { key } from "../../key.js";
 
 export const GameContext = createContext();
 
+// Component responsible for all functions and variables related to the BGA API
 export const GameProvider = (props) => {
+
+    // games holds the array of games returned by BGA fetch calls
     const [games, setGames] = useState([]);
 
-    // TAKE OUT LIMIT LATER
+    // ********TAKE OUT 5 GAME LIMIT LATER********
+    // Used in the searchPage
+    // Takes an object as an argument which holds filter information about the 
+    // fetch call. The url is constructed and then sent to the BGA API in order
+    // to receive and store the relevant games.
     const getGamesByFilters = (searchObject) => {
-        const name = searchObject.name;
-        const min_players = searchObject.min_players;
-        const categories = searchObject.categories;
-        const mechanics = searchObject.mechanics;
-        
-        return fetch(`https://api.boardgameatlas.com/api/search?limit=5&fuzzy_match=${name}&min_players=${min_players}&categories=${categories.map(category => category.id).join(",")}&mechanics=${mechanics.map(mechanic => mechanic.id).join(",")}&client_id=${key}`)
+        return fetch(`https://api.boardgameatlas.com/api/search?limit=5&fuzzy_match=${searchObject.name}&min_players=${searchObject.min_players}&categories=${searchObject.categories}&mechanics=${searchObject.mechanics}&client_id=${key}`)
         .then(response => response.json())
         .then(setGames);
     };
+
 
     const getGamesById = (ids) => {
         return fetch (`https://api.boardgameatlas.com/api/search?ids=${ids.map(id => id).join(",")}&client_id=${key}`)
--- src/components/searchPage/SearchFilters.js
+++ src/components/searchPage/SearchFilters.js
@@ -7,42 +7,51 @@ import { CategoryContext } from "../categories/CategoryProvider.js";
 import { MechanicContext } from "../mechanics/MechanicProvider.js"; 
 
 export const SearchFilters = () => {
+    // Pull context from these providers
     const { categories, getCategories } = useContext(CategoryContext);
     const { mechanics, getMechanics } = useContext(MechanicContext);
     const { games, getGamesByFilters } = useContext(GameContext);
-
+    
+    const [rangeValue, setRangeValue] = useState(1);
     const [isLoading, setIsLoading] = useState(true);
 
+    // Initialize the searchObject to be used for the fetch call to Board Game Atlas
     const [search, setSearch] = useState({
         name: "",
         min_players: 1,
-        category: "",
-        mechanic: ""
+        categories: null,
+        mechanics: null
     });
 
     const handleChange = (event) => {
         // Create a copy of the state object
-        const newSearch = { ...search}
+        const newSearch = { ...search};
 
         // Set the related property and its value to newSearch object
         newSearch[event.target.name] = event.target.value;
 
         // Update state of search
         setSearch(newSearch);
-    }
+
+        if(event.target.name === 'min_players'){
+            setRangeValue(event.target.value);
+        };
+    };
 
     const submitSearch = () => {
         console.log(search)
-        // Request game data from Board Game Atlas with the new search criteria
-        //getGamesByFilters(search)
-        //.then(console.log(games))
+        //Request game data from Board Game Atlas with the new search criteria
+        getGamesByFilters(search)
+        .then(console.log(games))
       };
 
       useEffect(() => {
         getCategories()
         .then(getMechanics)
         .then(setIsLoading(false));
-    },[]);
+    },
+    //eslint-disable-next-line
+    []);
 
     return (
         <form className="SearchFilters">
@@ -54,24 +63,25 @@ export const SearchFilters = () => {
                 </div>
             </fieldset>
             <fieldset>
-                <div className="sliderContainer">
+                <div className="range-slider">
                     <label htmlFor="minPlayers">Min Players</label>
-                    <input type="range" id="minPlayerSlider" name="min_players" onChange={handleChange} className="minPlayerSlider" min="1" max="8"></input>
+                    <input type="range" id="range-slider__range" name="min_players" onChange={handleChange} className="minPlayerSlider" min="1" max="8" defaultValue="1"></input>
+                    <span className="range-slider__value">{rangeValue}</span>
                 </div>
             </fieldset>
             <fieldset>
-                <div className="category">
-                    <label htmlFor="category">Category: </label>
-                    <select name="category" onChange={handleChange}>
+                <div className="categories">
+                    <label htmlFor="categories">Category: </label>
+                    <select name="categories" onChange={handleChange}>
                         <option value="">All Categories</option>
                         {categories.map(c => <option key={c.id} value={c.id}>{c.name}</option>)}
                     </select>
                 </div>
             </fieldset>
             <fieldset>
-                <div className="mechanic">
-                    <label htmlFor="mechanic">Mechanic: </label>
-                    <select name="mechanic" onChange={handleChange}>
+                <div className="mechanics">
+                    <label htmlFor="mechanics">Mechanic: </label>
+                    <select name="mechanics" onChange={handleChange}>
                         <option value="">All Mechanics</option>
                         {mechanics.map(m => <option key={m.id} value={m.id}>{m.name}</option>)}
                     </select>
