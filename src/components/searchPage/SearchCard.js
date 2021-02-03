50d2f0f4d148cf1c62665324c1408ebacec7efbe
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-02-03 16:43:47 -0600

    Added dynamic star rating to SearchCards

--- src/components/searchPage/SearchCard.js
+++ src/components/searchPage/SearchCard.js
@@ -5,33 +5,24 @@ import { Button } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
+import Grid from '@material-ui/core/Grid';
 
-// Import Modal
+// Import Details Modal
 import { SearchModal } from "./SearchModal.js";
+
+// Import Ratings Component
+import { SearchRating } from "./SearchRating.js";
 
 // Import CSS
 import "./Search.css";
 
 export const SearchCard = ({searchGame, saveUserGame, userGames}) => {
 
-    // Assigns specific className addition to game__averageRating based off of rating
-    // This will be used in CSS later to dictate the color used for that section
-    const checkRating = () => {
-        const rating = Math.floor(searchGame.average_user_rating);
-        switch(rating){
-            case 4:
-                return "great";
-            case 3:
-                return "good";
-            case 2:
-                return "ok";
-            case 1:
-                return "bad";
-            case 0:
-                return "unknown";
-            default:
-                return "unknown";
-        };
+    // Breaks ratings into 0.5 segments and uses Material UI to display a number
+    // of stars equal to the rating
+    const displayRating = () => {
+        const rating = (Math.round(searchGame.average_user_rating*2)/2);
+        return <SearchRating rating={rating}/> 
     };
 
     // This determines whether the current game has already been saved to the
@@ -54,7 +45,6 @@ export const SearchCard = ({searchGame, saveUserGame, userGames}) => {
     }
 
     const rules = () => {
-        console.log(searchGame.rules_url)
         if(searchGame.rules_url === null || searchGame.rules_url === undefined){
             return "No rules available"
         } else {
@@ -63,36 +53,40 @@ export const SearchCard = ({searchGame, saveUserGame, userGames}) => {
     }
 
     return (
-        <Card className="game">
-            <CardContent>
-                {/* Displays the game name */}
-                <h3 className="game__name">
-                    {searchGame.name}
-                </h3>
-                {/* Displays the game image */}
-                <div className="game__img">
-                    <img src={searchGame.images.small} alt={`Cover for ${searchGame.name}`} />
-                </div>
-                {/* Displays the game's average user rating */}
-                <div className={`game__averageRating--${checkRating()}`}>
-                    Average User Rating: {searchGame.average_user_rating.toFixed(2)} / 5.00
-                </div>
-                {/* Displays the game's minimum and maximum players */}
-                <div className="game__players">
-                    Players: {searchGame.min_players} - {searchGame.max_players}
-                </div>
-                <div className="game__msrp">
-                    {msrp()}
-                </div>
-                <div className="game__rules">
-                    {rules()}
-                </div>
-            </CardContent>
-            {/* Renders a button that lets the user save the game to their library if they haven't already */}
-            <CardActions>
-                <SearchModal searchGame={searchGame}/>
-                {saveButton()}
-            </CardActions>
+        <Card raised="true" className="game">
+                <CardContent>
+                    {/* Displays the game name */}
+                    <h3 className="game__name">
+                        {searchGame.name}
+                    </h3>
+                    {/* Displays the game image */}
+                    <div className="game__img">
+                        <img src={searchGame.images.small} alt={`Cover for ${searchGame.name}`} />
+                    </div>
+                    {/* Displays the game's average user rating */}
+                    <div className={`game__averageRating`}>
+                        Average User Rating: {displayRating()}
+                    </div>
+                    {/* Displays the game's minimum and maximum players */}
+                    <div className="game__players">
+                        Players: {searchGame.min_players} - {searchGame.max_players}
+                    </div>
+                    <div className="game__msrp">
+                        {msrp()}
+                    </div>
+                    <div className="game__rules">
+                        {rules()}
+                    </div>
+                </CardContent>
+            <Grid container justify="center">
+                <CardActions style={{}}>
+                    <SearchModal searchGame={searchGame}/>
+
+                    {/* Renders a button that lets the user save the game to their library if they haven't already */}
+                    {saveButton()}
+                </CardActions>
+            </Grid>
         </Card>
-)};
+    );
+};
 
