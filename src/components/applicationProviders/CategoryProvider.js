374e385d46e3ae1e2a6c225f237dd08f243a2b98
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-02-04 12:07:45 -0600

    Refactored MechanicProvider and CategoryProvider so that they are responsible for initialization of their states, respectively. Also made their functions more granular

--- src/components/applicationProviders/CategoryProvider.js
+++ src/components/applicationProviders/CategoryProvider.js
@@ -2,18 +2,29 @@ import React, { useState, createContext, useEffect } from "react";
 
 export const CategoryContext = createContext();
 
+// Responsible for getting, converting and providing game categories from an external API
 export const CategoryProvider = (props) => {
-    const [categories, setCategories] = useState([]);
-
      // Initialize the categories state
      useEffect(() => getCategories(),
     //eslint-disable-next-line
     []);
 
-    const getCategories = () => {
+    // Loads response from external API for categories
+    // Converts the response to a JS Object 
+    // Returns resulting data
+    const loadCategories = () => {
         return fetch("https://www.boardgameatlas.com/api/game/categories?pretty=true&client_id=JLBr5npPhV")
         .then(response => response.json())
-        .then((data) => setCategories(data.categories));
+        .then(data => data.categories);
+    };
+
+    // Used to store and update state of categories
+    const [categories, setCategories] = useState([]);
+
+    // Combines the loading and setting of categories
+    const getCategories = () => {
+        loadCategories()
+        .then(setCategories);
     };
 
     return (
--- src/components/applicationProviders/MechanicProvider.js
+++ src/components/applicationProviders/MechanicProvider.js
@@ -3,17 +3,28 @@ import React, { useState, createContext, useEffect } from "react";
 export const MechanicContext = createContext();
 
 export const MechanicProvider = (props) => {
-    const [mechanics, setMechanics] = useState([]);
 
-     // Initialize the mechanics states
-     useEffect(() => (getMechanics),
+     // Initialize the mechanics state
+     useEffect(() => (getMechanics()),
     //eslint-disable-next-line
     []);
 
-    const getMechanics = () => {
+    // Loads response from external API for mechanics
+    // Converts the response to a JS Object 
+    // Returns resulting data
+    const loadMechanics = () => {
         return fetch("https://www.boardgameatlas.com/api/game/mechanics?pretty=true&client_id=JLBr5npPhV")
         .then(response => response.json())
-        .then((data) => setMechanics(data.mechanics));
+        .then(data => data.mechanics);
+    };
+
+    // Used to store and update state of mechanics
+    const [mechanics, setMechanics] = useState([]);
+
+    // Combines the loading and setting of mechanics
+    const getMechanics = () => {
+        loadMechanics()
+        .then(setMechanics);
     };
 
     return (
